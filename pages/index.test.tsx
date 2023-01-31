c459ec96b10120bbbadaca4f439c1c97c05b6493
Nathan <79507956+CUexter@users.noreply.github.com>
2023-02-01 02:50:43 +0800

    chore: disable app directory

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Index from "./_index";
+import Index from "./index";
 import "@testing-library/jest-dom";
 
 describe.skip("Home", () => {
