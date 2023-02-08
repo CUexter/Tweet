994f18c8f5399832928f9fb83cfe91f361839e51
Nathan <79507956+CUexter@users.noreply.github.com>
2023-02-08 18:23:01 +0800

    feat: migrating to t3-stack

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,13 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Index from "@/pages/index";
-import "@testing-library/jest-dom";
 
 describe("Home", () => {
-  it("renders a heading", () => {
+  test("renders a heading", () => {
     render(<Index />);
 
-    const heading = screen.getByRole("heading", {
-      name: /testing/i,
+    const heading = screen.getByRole("p", {
+      name: /Sign In/i,
     });
 
     expect(heading).toBeInTheDocument();
