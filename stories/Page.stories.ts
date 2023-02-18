10bd5b97377f0b1d01a0a32f321c7740fa587e91
CUexter <CUexter@users.noreply.github.com>
2023-02-18 20:23:22 +0000

    style: :robot: prettier code by github action

--- stories/Page.stories.ts
+++ stories/Page.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { within, userEvent } from "@storybook/testing-library";
+
+import { userEvent, within } from "@storybook/testing-library";
 
 import { Page } from "./Page";
 
