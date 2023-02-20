8c77e727d2558ebeb69ce048eae63a65a12cc736
Nathan <79507956+CUexter@users.noreply.github.com>
2023-02-20 20:07:40 +0800

    feat: mantine support

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,15 +4,18 @@ import { type AppType } from "next/app";
 
 import "@/styles/globals.css";
 import { api } from "@/utils/api";
+import { MantineProvider } from "@mantine/core";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
   );
 };
 
