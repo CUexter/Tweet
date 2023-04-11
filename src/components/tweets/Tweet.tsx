8f42862055e4fce60c2c9ae8f03de5ddb6a4f989
1155163071@link.cuhk.edu.hk <1155163071@link.cuhk.edu.hk>
2023-04-11 22:04:19 +0800

    improved tweet component design

--- src/components/tweets/Tweet.tsx
+++ src/components/tweets/Tweet.tsx
@@ -1,13 +1,35 @@
 import type { TweetData } from "@/types/tweet";
 
 import { api } from "@/utils/api";
-import { Card } from "@mantine/core";
+import {
+  ActionIcon,
+  Avatar,
+  Card,
+  Group,
+  Text,
+  createStyles,
+  rem,
+} from "@mantine/core";
+import {
+  IconArrowAutofitLeft,
+  IconArrowBackUp,
+  IconHeart,
+} from "@tabler/icons-react";
+import Head from "next/head";
+
+const useStyles = createStyles((theme) => ({
+  body: {
+    paddingLeft: rem(54),
+    paddingTop: theme.spacing.sm,
+  },
+}));
 
 interface TweetProp {
   tweetID: string;
   tweetData?: TweetData;
 }
 const Tweet = ({ tweetID, tweetData }: TweetProp) => {
+  const { classes } = useStyles();
   // if they didn't pass in any tweetData, get it
   ({ data: tweetData } = api.tweet.getTweet.useQuery(
     { id: tweetID },
@@ -22,10 +44,66 @@ const Tweet = ({ tweetID, tweetData }: TweetProp) => {
   if (!tweetData) {
     return <>It not here</>;
   }
+
+  const likes = tweetData.Likes.length;
+  let retweets = 0;
+  let replies = 0;
+  for (const tweet of tweetData.Retweeted_by) {
+    if (tweet.on_profile) {
+      retweets++;
+    } else {
+      replies++;
+    }
+  }
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      {tweetData?.user.display_name}: {tweetData?.TweetText[0]?.tweet_text}
-    </Card>
+    <>
+      <Head>
+        <title>
+          {tweetData.user.display_name}: &quot;
+          {tweetData.TweetText[0]?.tweet_text}&quot;
+        </title>
+        <meta name="description" content="for CSCI3100" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="mx-auto w-1/3">
+        <Card withBorder>
+          <Group>
+            <Avatar src={tweetData.user.image} color={"yellow"} radius="xl" />
+            <div>
+              <Text size="sm">{tweetData.user.display_name}</Text>
+              <Text size="xs" color="dimmed">
+                {tweetData.created_at.toString()}
+              </Text>
+            </div>
+          </Group>
+          <Text className={classes.body} size="sm">
+            {tweetData.TweetText[0]?.tweet_text}
+          </Text>
+          <Group className="justify-evenly pt-4">
+            <Group>
+              <Text size="sm">{replies}</Text>
+              <ActionIcon onClick={() => console.log("placeholder for reply")}>
+                <IconArrowBackUp size="1.5rem" />
+              </ActionIcon>
+            </Group>
+            <Group>
+              <Text size="sm">{retweets}</Text>
+              <ActionIcon
+                onClick={() => console.log("placeholder for retweet")}
+              >
+                <IconArrowAutofitLeft size="1.5rem" />
+              </ActionIcon>
+            </Group>
+            <Group>
+              <Text size="sm">{likes}</Text>
+              <ActionIcon onClick={() => console.log("placeholder for like")}>
+                <IconHeart size="1.5rem" />
+              </ActionIcon>
+            </Group>
+          </Group>
+        </Card>
+      </div>
+    </>
   );
 };
 
