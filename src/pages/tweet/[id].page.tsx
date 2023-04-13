882e964aebf84cda0fc21f07938620e44ed8126e
Falcon <falfzz28@gmail.com>
2023-04-13 23:49:54 +0800

    fix: tweet title

--- src/pages/tweet/[id].page.tsx
+++ src/pages/tweet/[id].page.tsx
@@ -1,6 +1,7 @@
 import Tweet from "@/components/tweets/Tweet";
 import { api } from "@/utils/api";
 import { Text } from "@mantine/core";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 // interface TweetPageProp {}
@@ -12,13 +13,21 @@ const TweetPage = () => {
   if (tid === "error") return <>you shouldnt be here</>;
   const { data: mainTweet } = api.tweet.getTweet.useQuery({ id: tid });
 
-  if (!mainTweet) return <>It not here</>;
+  if (!mainTweet) return <>No tweet here</>;
 
   const { replied_by: replies } = mainTweet;
   const replies_id = replies.map((reply) => reply.id);
 
   return (
     <>
+      <Head>
+        <title>
+          {mainTweet.user.display_name}: &quot;
+          {mainTweet.TweetText[0]?.tweet_text}&quot;
+        </title>
+        <meta name="description" content="for CSCI3100" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="mx-auto w-3/4 h-screen">
         {/* Main tweet */}
         <Tweet tweetID={tid} />
